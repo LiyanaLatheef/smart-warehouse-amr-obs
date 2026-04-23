c16deddbda29debd8bb856bc3e9954c7b557af0a
LiyanaLatheef <liyanalatheef17@gmail.com>
2026-04-23 14:15:59 +0530

    Update motor_control.ino
    changed final arduino code

--- firmware/motor_control.ino
+++ firmware/motor_control.ino
@@ -6,39 +6,50 @@
 #define ENB 10
 
 void setup() {
-  Serial.begin(57600);
+  // MUST match the Pi's baud rate
+  Serial.begin(115200); 
+  
   pinMode(IN1, OUTPUT); pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT); pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT); pinMode(ENB, OUTPUT);
-  analogWrite(ENA, 0);
-  analogWrite(ENB, 0);
+  
+  // Safety: Start stopped
+  stopMotors();
 }
 
 void loop() {
-  if (Serial.available() >= 2) {
-    int left_raw  = Serial.read();
-    int right_raw = Serial.read();
-    float left_limit  = 1.5;
-    float right_limit = 1.5;
-    if (left_raw > 130) {
-      digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
-      analogWrite(ENA, constrain((int)((left_raw - 127) * left_limit), 0, 255));
-    } else if (left_raw < 124) {
-      digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
-      analogWrite(ENA, constrain((int)((127 - left_raw) * left_limit), 0, 255));
-    } else {
-      digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
-      analogWrite(ENA, 0);
+  if (Serial.available() > 0) {
+    char cmd = Serial.read();
+    
+    // Blink LED on pin 13 so you can see the command arriving
+    digitalWrite(13, HIGH); 
+
+    if (cmd == 'F') { // Forward
+       digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+       digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+       analogWrite(ENA, 75); analogWrite(ENB, 75);
+    } 
+    else if (cmd == 'L') { // Left Turn
+       digitalWrite(IN1, LOW);  digitalWrite(IN2, HIGH);
+       digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
+       analogWrite(ENA, 75); analogWrite(ENB, 75);
     }
-    if (right_raw > 130) {
-      digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-      analogWrite(ENB, constrain((int)((right_raw - 127) * right_limit), 0, 255));
-    } else if (right_raw < 124) {
-      digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
-      analogWrite(ENB, constrain((int)((127 - right_raw) * right_limit), 0, 255));
-    } else {
-      digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
-      analogWrite(ENB, 0);
+    else if (cmd == 'R') { // Right Turn
+       digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
+       digitalWrite(IN3, LOW);  digitalWrite(IN4, HIGH);
+       analogWrite(ENA, 75); analogWrite(ENB, 75);
     }
+    else if (cmd == 'S') { // Stop
+       stopMotors();
+    }
+    
+    delay(10);
+    digitalWrite(13, LOW);
   }
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW); digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0); analogWrite(ENB, 0);
 }
